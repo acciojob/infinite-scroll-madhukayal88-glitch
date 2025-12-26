d9f86f5e6fb8026114f7c1589edff8bed6a62144
madhukayal88-glitch <madhukayal88@gmail.com>
2025-12-26 06:22:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,24 @@
 const list = document.getElementById("infi-list");
 
-// 1. Initial Setup: Add 10 items by default
-let itemCount = 10;
+// Start from 0 so numbering is correct
+let itemCount = 0;
 
+// Function to add items
 function addItems(count) {
   for (let i = 0; i < count; i++) {
     const li = document.createElement("li");
-    li.textContent = `Item ${++itemCount}`;
+    itemCount++;
+    li.textContent = `Item ${itemCount}`;
     list.appendChild(li);
   }
 }
 
-// Initial call to populate the list
+// 1. Initial Setup: Add 10 items by default
 addItems(10);
 
 // 2. Functionality: Detect end of scroll
 list.addEventListener("scroll", () => {
-  // scrollTop: How much the user has scrolled from the top
-  // clientHeight: The height of the visible part of the list
-  // scrollHeight: The total height of the content (including hidden part)
-  
-  if (list.scrollTop + list.clientHeight >= list.scrollHeight - 1) {
+  if (list.scrollTop + list.clientHeight >= list.scrollHeight) {
     // 3. Requirement: Add 2 more items automatically
     addItems(2);
   }
